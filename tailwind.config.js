38787aa0cefe6ac33667ba8c89b8e4b4f466886e
santosh325 <santoshthapa.ts12@gmail.com>
2023-02-16 15:21:05 +0545

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  theme: {
+    fontFamily: {
+      sans: ['Mulish', 'sans-serif'],
+      mono: ['Rokkitt', 'monospace']
+    }
+  }
+}
